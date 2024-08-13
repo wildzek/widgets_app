f16adc8e593fff1a2131da625745418f60f40ca0
wildzek <dev@rogertorrent.com>
2024-08-13 19:00:10 +0200

    Routes with name implementation

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu_items/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -54,6 +56,7 @@ class _CustomListTile extends StatelessWidget {
         //   ),
         // );
         //Navigator.pushNamed(context, menuItem.link);
+        //context.pushNamed(CardsScreen.name);
         context.push(menuItem.link);
       },
     );
