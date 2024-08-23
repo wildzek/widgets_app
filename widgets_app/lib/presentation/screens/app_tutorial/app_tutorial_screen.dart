7979280bd0d73aaa450fff6b0b3b0e26ea0eb600
wildzek <dev@rogertorrent.com>
2024-08-23 15:33:52 +0200

    Images properly uploaded and configured

--- widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -2,23 +2,23 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 
 class SlideInfo {
   final String title;
-  final String imageUrl;
   final String caption;
+  final String imageUrl;
 
-  SlideInfo(this.title, this.imageUrl, this.caption);
+  SlideInfo(this.title, this.caption, this.imageUrl);
 }
 
 final slides = <SlideInfo>[
   SlideInfo(
       'Busca la comida',
       'Aliquip aliquip sit nulla eu eiusmod anim deserunt et deserunt fugiat aute officia.',
-      'assets/images/1.png'),
+      'assets/Images/1.png'),
   SlideInfo('Entrega rápida', 'Fugiat eu eu qui cillum ea deserunt eu.',
-      'assets/images/2.png'),
+      'assets/Images/2.png'),
   SlideInfo(
       'Disfruta la comida',
       'Excepteur nisi tempor officia cupidatat velit et irure reprehenderit deserunt amet anim in veniam ea.',
-      'assets/images/3.png'),
+      'assets/Images/3.png'),
 ];
 
 class AppTutorialScreen extends StatelessWidget {
@@ -30,15 +30,13 @@ class AppTutorialScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: slides.map(
-          (slideData) => _Slide(
-            title: slideData.title,
-            caption: slideData.caption,
-            imageUrl: slideData.imageUrl
-          )
-          ).toList()
-        ),
+          physics: const BouncingScrollPhysics(),
+          children: slides
+              .map((slideData) => _Slide(
+                  title: slideData.title,
+                  caption: slideData.caption,
+                  imageUrl: slideData.imageUrl))
+              .toList()),
     );
   }
 }
@@ -48,10 +46,25 @@ class _Slide extends StatelessWidget {
   final String caption;
   final String imageUrl;
 
-  const _Slide({required this.title, required this.caption, required this.imageUrl});
+  const _Slide(
+      {required this.title, required this.caption, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Image(image: AssetImage(imageUrl)),
+            const SizedBox(height: 20),
+            Text(title),
+            const SizedBox(height: 10),
+            Text(caption),
+            
+            ],
+          ),
+        ));
   }
 }
