8bffe4acd747053487b60da016e744a2eb1ffc64
wildzek <dev@rogertorrent.com>
2024-08-28 21:06:15 +0200

    Created a side bar menu and configured with the titles of every page

---
+++ widgets_app/lib/presentation/widgets/side_menu.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/menu_items/menu_items.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  int navDrawerIndex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+    
+
+    return NavigationDrawer(
+        selectedIndex: navDrawerIndex,
+        onDestinationSelected: (value) {
+          setState(() {
+            navDrawerIndex = value;
+          });
+        },
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+            child: Text('Main'),
+          ),
+          ...appMenuItems.map(
+            
+            (item) => NavigationDrawerDestination(
+              icon: Icon(item.icon),
+              label: Text(item.title),
+            ),
+          )
+        ]);
+  }
+}
