a71741d4f159d1d2a93d6fd762e59668d0ea06a4
wildzek <dev@rogertorrent.com>
2024-08-28 21:20:05 +0200

    Navigation from the side bar menu to pages configured

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -10,7 +10,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
--- widgets_app/lib/presentation/widgets/side_menu.dart
+++ widgets_app/lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu_items/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
@@ -11,11 +12,9 @@ class SideMenu extends StatefulWidget {
 class _SideMenuState extends State<SideMenu> {
   int navDrawerIndex = 0;
 
-
   @override
   Widget build(BuildContext context) {
     final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
-    
 
     return NavigationDrawer(
         selectedIndex: navDrawerIndex,
@@ -23,44 +22,35 @@ class _SideMenuState extends State<SideMenu> {
           setState(() {
             navDrawerIndex = value;
           });
+
+          final menuItem = appMenuItems[value];
+          context.push(menuItem.link);
         },
         children: [
           Padding(
             padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
-            child: Text('Main'),
+            child: const Text('Main'),
           ),
-          ...appMenuItems
-            .sublist(0,3)
-
-            .map(
-            (item) => NavigationDrawerDestination(
-              icon: Icon(item.icon),
-              label: Text(item.title),
-            ),
-          ),
-
+          ...appMenuItems.sublist(0, 3).map(
+                (item) => NavigationDrawerDestination(
+                  icon: Icon(item.icon),
+                  label: Text(item.title),
+                ),
+              ),
           const Padding(
             padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
             child: Divider(),
           ),
-
           const Padding(
             padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
             child: Text('More options'),
           ),
-
-          ...appMenuItems
-            .sublist(3)
-
-            .map(
-            (item) => NavigationDrawerDestination(
-              icon: Icon(item.icon),
-              label: Text(item.title),
-            ),
-          ),
-
-
-
+          ...appMenuItems.sublist(3).map(
+                (item) => NavigationDrawerDestination(
+                  icon: Icon(item.icon),
+                  label: Text(item.title),
+                ),
+              ),
         ]);
   }
 }
