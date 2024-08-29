feeb477bf94356d0218788860505a0292eb92976
wildzek <dev@rogertorrent.com>
2024-08-29 21:38:32 +0200

    Dark/light mode button configured using riverpod

--- widgets_app/lib/presentation/screens/counter/counter_screen.dart
+++ widgets_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'Counter Screen';
@@ -9,18 +10,28 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final themeColor = ref.watch(isDarkmodeProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter Screen'),
-      ),
+      appBar: AppBar(title: const Text('Counter Screen'), actions: [
+        IconButton(
+          icon: Icon(themeColor ? Icons.light_mode_rounded : Icons.dark_mode_rounded),
+          onPressed: () {
+            ref.read(isDarkmodeProvider.notifier).state = !themeColor;
+          },
+          )
+      ]),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge)
+          Text('Valor: $clickCounter',
+              style: Theme.of(context).textTheme.titleLarge)
         ]),
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(counterProvider.notifier).state++;
+          },
+          child: const Icon(Icons.add)),
     );
   }
 }
